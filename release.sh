9154db91045509556fa45b9c43a6a5fd4a27b6ac
cwenger <craig.wenger@gmail.com>
2013-05-14 22:13:31 +0000

    Add script for automating releases with Linux.

---
+++ release.sh
@@ -0,0 +1,5 @@
+mkdir release
+mkdir "release/Morpheus (Linux)"
+mdtool build "Morpheus/Morpheus (MonoDevelop).sln" -t:Clean -c:Release
+mdtool build "Morpheus/Morpheus (MonoDevelop).sln" -t:Build -c:Release
+cp -r "Morpheus/Morpheus (command line)/bin/Release/." "release/Morpheus (Linux)"
