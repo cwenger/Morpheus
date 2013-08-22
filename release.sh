d7805df48761111cdce35e5779fa003750fe88f9
cwenger <cwenger@9f24ff98-5b6e-4570-93d2-b811ea2ddf1e>
2013-08-22 23:58:13 +0000

    Modifying Linux release script to automatically tar and gzip.

--- release.sh
+++ release.sh
@@ -3,3 +3,7 @@ mkdir "release/Morpheus (Linux)"
 mdtool build "Morpheus/Morpheus (MonoDevelop).sln" -t:Clean -c:Release
 mdtool build "Morpheus/Morpheus (MonoDevelop).sln" -t:Build -c:Release
 cp -r "Morpheus/Morpheus (command line)/bin/Release/." "release/Morpheus (Linux)"
+
+cd release
+tar -czf "Morpheus (Linux).tar.gz" "Morpheus (Linux)"
+
