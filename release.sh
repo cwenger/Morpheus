5d904131ef2da8bc127205639a7836067d1d137e
cwenger <craig.wenger@gmail.com>
2013-08-23 00:22:31 +0000

    Add Linux revision numbers to releases.

--- release.sh
+++ release.sh
@@ -1,9 +1,11 @@
+revision=$(svnversion -n)
+
 mkdir release
-mkdir "release/Morpheus (Linux)"
+mkdir "release/Morpheus (Linux) revision $revision"
 mdtool build "Morpheus/Morpheus (MonoDevelop).sln" -t:Clean -c:Release
 mdtool build "Morpheus/Morpheus (MonoDevelop).sln" -t:Build -c:Release
-cp -r "Morpheus/Morpheus (command line)/bin/Release/." "release/Morpheus (Linux)"
+cp -r "Morpheus/Morpheus (command line)/bin/Release/." "release/Morpheus (Linux) revision $revision"
 
 cd release
-tar -czf "Morpheus (Linux).tar.gz" "Morpheus (Linux)"
+tar -czf "Morpheus (Linux) revision $revision.tar.gz" "Morpheus (Linux) revision $revision"
 
