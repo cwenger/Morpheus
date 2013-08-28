f6ab9628c3202e5228e81d3dce2ef268981251af
cwenger <craig.wenger@gmail.com>
2013-08-28 03:42:22 +0000

    Removing revision numbers from build files as they break links.

--- release.bat
+++ release.bat
@@ -1,22 +1,19 @@
-for /f %%i in ('svnversion -n') do set revision=%%i
-
 mkdir release
-mkdir "release\Morpheus revision %revision%"
+mkdir release\Morpheus
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" Morpheus\Morpheus.sln /rebuild Release /project "Morpheus (command line)"
-xcopy "Morpheus\Morpheus (command line)\bin\Release\*" /EXCLUDE:exclude.txt "release\Morpheus revision %revision%"
-mkdir "release\Morpheus (Agilent 32-bit) revision %revision%"
+xcopy "Morpheus\Morpheus (command line)\bin\Release\*" /EXCLUDE:exclude.txt release\Morpheus
+mkdir "release\Morpheus (Agilent 32-bit)"
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" "Morpheus\Morpheus (Agilent).sln" /rebuild Release /project "Morpheus (Agilent 32-bit command line)"
-xcopy "Morpheus\Morpheus (command line)\bin\x86\Release\*" /EXCLUDE:exclude.txt "release\Morpheus (Agilent 32-bit) revision %revision%"
-mkdir "release\Morpheus (Agilent 64-bit) revision %revision%"
+xcopy "Morpheus\Morpheus (command line)\bin\x86\Release\*" /EXCLUDE:exclude.txt "release\Morpheus (Agilent 32-bit)"
+mkdir "release\Morpheus (Agilent 64-bit)"
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" "Morpheus\Morpheus (Agilent).sln" /rebuild Release /project "Morpheus (Agilent 64-bit command line)"
-xcopy "Morpheus\Morpheus (command line)\bin\x64\Release\*" /EXCLUDE:exclude.txt "release\Morpheus (Agilent 64-bit) revision %revision%"
-mkdir "release\Morpheus (Thermo) revision %revision%"
+xcopy "Morpheus\Morpheus (command line)\bin\x64\Release\*" /EXCLUDE:exclude.txt "release\Morpheus (Agilent 64-bit)"
+mkdir "release\Morpheus (Thermo)"
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" "Morpheus\Morpheus (Thermo).sln" /rebuild Release /project "Morpheus (Thermo command line)"
-xcopy "Morpheus\Morpheus (command line)\bin\Thermo\Release\*" /EXCLUDE:exclude.txt "release\Morpheus (Thermo) revision %revision%"
-
+xcopy "Morpheus\Morpheus (command line)\bin\Thermo\Release\*" /EXCLUDE:exclude.txt "release\Morpheus (Thermo)"
 
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" ZipFolder\ZipFolder.sln /rebuild Release /project "ZipFolder"
-ZipFolder\bin\Release\ZipFolder.exe "release\Morpheus revision %revision%"
-ZipFolder\bin\Release\ZipFolder.exe "release\Morpheus (Agilent 32-bit) revision %revision%"
-ZipFolder\bin\Release\ZipFolder.exe "release\Morpheus (Agilent 64-bit) revision %revision%"
-ZipFolder\bin\Release\ZipFolder.exe "release\Morpheus (Thermo) revision %revision%"
+ZipFolder\bin\Release\ZipFolder.exe release\Morpheus
+ZipFolder\bin\Release\ZipFolder.exe "release\Morpheus (Agilent 32-bit)"
+ZipFolder\bin\Release\ZipFolder.exe "release\Morpheus (Agilent 64-bit)"
+ZipFolder\bin\Release\ZipFolder.exe "release\Morpheus (Thermo)"
--- release.sh
+++ release.sh
@@ -1,11 +1,9 @@
-revision=$(svnversion -n)
-
 mkdir release
-mkdir "release/Morpheus (Linux) revision $revision"
+mkdir "release/Morpheus (Linux)"
 mdtool build "Morpheus/Morpheus (MonoDevelop).sln" -t:Clean -c:Release
 mdtool build "Morpheus/Morpheus (MonoDevelop).sln" -t:Build -c:Release
-cp -r "Morpheus/Morpheus (command line)/bin/Release/." "release/Morpheus (Linux) revision $revision"
+cp -r "Morpheus/Morpheus (command line)/bin/Release/." "release/Morpheus (Linux)"
 
 cd release
-tar -czf "Morpheus (Linux) revision $revision.tar.gz" "Morpheus (Linux) revision $revision"
+tar -czf "Morpheus (Linux).tar.gz" "Morpheus (Linux)"
 
