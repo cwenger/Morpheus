4823841d22fc8f351c95ce78b180c463ce1e1d10
cwenger <craig.wenger@gmail.com>
2013-12-11 00:55:07 +0000

    Add folder for revision number.

--- release.bat
+++ release.bat
@@ -5,22 +5,27 @@ rmdir /s /q "Morpheus\Morpheus (command line)\bin"
 for /f %%i in ('svnversion -n') do set revision=%%i
 
 mkdir release
-copy nul "release\revision %revision%"
-mkdir release\Morpheus
+mkdir "release\revision %revision%"
+mkdir "release\revision %revision%\Morpheus"
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" Morpheus\Morpheus.sln /rebuild Release /project "Morpheus (command line)"
-xcopy "Morpheus\Morpheus (command line)\bin\Release\*" /EXCLUDE:exclude.txt release\Morpheus
-mkdir "release\Morpheus (Agilent 32-bit)"
+xcopy "Morpheus\Morpheus (command line)\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus"
+mkdir "release\revision %revision%\Morpheus (Agilent 32-bit)"
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" "Morpheus\Morpheus (Agilent).sln" /rebuild Release /project "Morpheus (Agilent 32-bit command line)"
-xcopy "Morpheus\Morpheus (command line)\bin\Agilent\32-bit\Release\*" /EXCLUDE:exclude.txt "release\Morpheus (Agilent 32-bit)"
-mkdir "release\Morpheus (Agilent 64-bit)"
+xcopy "Morpheus\Morpheus (command line)\bin\Agilent\32-bit\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (Agilent 32-bit)"
+mkdir "release\revision %revision%\Morpheus (Agilent 64-bit)"
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" "Morpheus\Morpheus (Agilent).sln" /rebuild Release /project "Morpheus (Agilent 64-bit command line)"
-xcopy "Morpheus\Morpheus (command line)\bin\Agilent\64-bit\Release\*" /EXCLUDE:exclude.txt "release\Morpheus (Agilent 64-bit)"
-mkdir "release\Morpheus (Thermo)"
+xcopy "Morpheus\Morpheus (command line)\bin\Agilent\64-bit\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (Agilent 64-bit)"
+mkdir "release\revision %revision%\Morpheus (Thermo)"
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" "Morpheus\Morpheus (Thermo).sln" /rebuild Release /project "Morpheus (Thermo command line)"
-xcopy "Morpheus\Morpheus (command line)\bin\Thermo\Release\*" /EXCLUDE:exclude.txt "release\Morpheus (Thermo)"
+xcopy "Morpheus\Morpheus (command line)\bin\Thermo\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (Thermo)"
 
 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" ZipFolder\ZipFolder.sln /rebuild Release /project "ZipFolder"
-ZipFolder\bin\Release\ZipFolder.exe release\Morpheus
-ZipFolder\bin\Release\ZipFolder.exe "release\Morpheus (Agilent 32-bit)"
-ZipFolder\bin\Release\ZipFolder.exe "release\Morpheus (Agilent 64-bit)"
-ZipFolder\bin\Release\ZipFolder.exe "release\Morpheus (Thermo)"
+ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus"
+ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (Agilent 32-bit)"
+ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (Agilent 64-bit)"
+ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (Thermo)"
+
+rmdir /s /q "release\revision %revision%\Morpheus"
+rmdir /s /q "release\revision %revision%\Morpheus (Agilent 32-bit)"
+rmdir /s /q "release\revision %revision%\Morpheus (Agilent 64-bit)"
+rmdir /s /q "release\revision %revision%\Morpheus (Thermo)"
