7533b701eb4a94a5d24790bb1346b0916cee9f78
cwenger <craig.wenger@gmail.com>
2013-12-11 00:56:05 +0000

    Add folder for revision number.

--- release.sh
+++ release.sh
@@ -1,12 +1,17 @@
+revision=$(svnversion -n)
+
 rm -r release
 rm -r "Morpheus/Morpheus (command line)/bin"
 
 mkdir release
-mkdir "release/Morpheus (Linux)"
+mkdir "release/revision $revision"
+mkdir "release/revision $revision/Morpheus (Linux)"
 mdtool build "Morpheus/Morpheus (MonoDevelop).sln" -t:Clean -c:Release
 mdtool build "Morpheus/Morpheus (MonoDevelop).sln" -t:Build -c:Release
-cp -r "Morpheus/Morpheus (command line)/bin/Release/." "release/Morpheus (Linux)"
+cp -r "Morpheus/Morpheus (command line)/bin/Release/." "release/revision $revision/Morpheus (Linux)"
 
-cd release
+cd "release/revision $revision"
 tar -czf "Morpheus (Linux).tar.gz" "Morpheus (Linux)"
+
+rm -r "Morpheus (Linux)"
 
