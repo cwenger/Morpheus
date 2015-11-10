3addb27c6bcea3bc82d653a2653d0b5a30103132
cwenger <cwenger@9f24ff98-5b6e-4570-93d2-b811ea2ddf1e>
2015-11-10 19:01:53 +0000

    Added NON_MULTITHREADED flag for loading mzML. Added m/z sorting of peaks before charge state assignment and deisotoping. Better handling of polarity = 0 in mzML.

--- Morpheus/Utilities.cs
+++ Morpheus/Utilities.cs
@@ -6,11 +6,15 @@ namespace Morpheus
     {
         public static double MassFromMZ(double mz, int charge)
         {
-            return mz * Math.Abs(charge) - charge * Constants.PROTON_MASS;
+            return charge == 0 ? mz : mz * Math.Abs(charge) - charge * Constants.PROTON_MASS;
         }
 
         public static double MZFromMass(double mass, int charge)
         {
+            if(charge == 0)
+            {
+                throw new ArgumentOutOfRangeException("Charge cannot be zero.");
+            }
             return (mass + charge * Constants.PROTON_MASS) / Math.Abs(charge);
         }
     }
