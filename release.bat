58a7cbc491e527dc9f42994ac18c79fa752cdfb4
cwenger <cwenger@9f24ff98-5b6e-4570-93d2-b811ea2ddf1e>
2015-11-24 16:24:42 +0000

    Adding 'mzML' where needed in release scripts.

--- release.bat
+++ release.bat
@@ -8,8 +8,8 @@ mkdir release
 mkdir "release\revision %revision%"
 mkdir "release\revision %revision%\Morpheus"
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Morpheus\Morpheus (mzML).sln" /rebuild Release
-xcopy /y "Morpheus\mzML\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus"
-xcopy /y "Morpheus\mzML\command line\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus"
+xcopy /y "Morpheus\mzML\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (mzML)"
+xcopy /y "Morpheus\mzML\command line\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (mzML)"
 mkdir "release\revision %revision%\Morpheus (Agilent 32-bit)"
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Morpheus\Morpheus (Agilent).sln" /rebuild "Release|All"
 xcopy /y "Morpheus\Agilent\32-bit\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (Agilent 32-bit)"
@@ -23,12 +23,12 @@ xcopy /y "Morpheus\Thermo\bin\Release\*" /EXCLUDE:exclude.txt "release\revision 
 xcopy /y "Morpheus\Thermo\command line\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (Thermo)"
 
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZipFolder\ZipFolder.sln /rebuild Release
-ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus"
+ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (mzML)"
 ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (Agilent 32-bit)"
 ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (Agilent 64-bit)"
 ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (Thermo)"
 
-rmdir /s /q "release\revision %revision%\Morpheus"
+rmdir /s /q "release\revision %revision%\Morpheus (mzML)"
 rmdir /s /q "release\revision %revision%\Morpheus (Agilent 32-bit)"
 rmdir /s /q "release\revision %revision%\Morpheus (Agilent 64-bit)"
 rmdir /s /q "release\revision %revision%\Morpheus (Thermo)"
--- release.sh
+++ release.sh
@@ -5,13 +5,13 @@ find . -name "bin" -type d -exec rm -r "{}" \;
 
 mkdir release
 mkdir "release/revision $revision"
-mkdir "release/revision $revision/Morpheus (Linux)"
-mdtool build "Morpheus/Morpheus (Mono).sln" -t:Clean -c:Release
-mdtool build "Morpheus/Morpheus (Mono).sln" -t:Build -c:Release
-rsync -a --exclude=*.mdb "Morpheus/mzML/Mono/bin/Release/." "release/revision $revision/Morpheus (Linux)"
-rsync -a --exclude=*.mdb "Morpheus/mzML/Mono/command line/bin/Release/." "release/revision $revision/Morpheus (Linux)"
+mkdir "release/revision $revision/Morpheus (mzML Linux)"
+mdtool build "Morpheus/Morpheus (mzML Mono).sln" -t:Clean -c:Release
+mdtool build "Morpheus/Morpheus (mzML Mono).sln" -t:Build -c:Release
+rsync -a --exclude=*.mdb "Morpheus/mzML/Mono/bin/Release/." "release/revision $revision/Morpheus (mzML Linux)"
+rsync -a --exclude=*.mdb "Morpheus/mzML/Mono/command line/bin/Release/." "release/revision $revision/Morpheus (mzML Linux)"
 
 cd "release/revision $revision"
-tar -czf "Morpheus (Linux).tar.gz" "Morpheus (Linux)"
+tar -czf "Morpheus (Linux).tar.gz" "Morpheus (mzML Linux)"
 
 rm -r "Morpheus (Linux)"
