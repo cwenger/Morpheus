bba62bdb8f7791615efd0aaa7b543db6dad294b5
Craig Wenger <craig.wenger@gmail.com>
2016-01-29 18:50:49 -0500

    Removing spaces from Mono release .tar.gz file.

--- release.sh
+++ release.sh
@@ -5,13 +5,16 @@ find . -name "bin" -type d -exec rm -r "{}" \;
 
 mkdir release
 mkdir "release/revision $revision"
-mkdir "release/revision $revision/Morpheus (mzML Linux)"
+
+mkdir "release/revision $revision/Morpheus (mzML Mono)"
 mdtool build "Morpheus/Morpheus (mzML Mono).sln" -t:Clean -c:Release
 mdtool build "Morpheus/Morpheus (mzML Mono).sln" -t:Build -c:Release
-rsync -a --exclude=*.mdb "Morpheus/mzML/Mono/bin/Release/." "release/revision $revision/Morpheus (mzML Linux)"
-rsync -a --exclude=*.mdb "Morpheus/mzML/Mono/command line/bin/Release/." "release/revision $revision/Morpheus (mzML Linux)"
+cp -f /dev/null "release/revision $revision/Morpheus (mzML Mono)/revision $revision"
+rsync -a --exclude=*.mdb "Morpheus/mzML/Mono/bin/Release/." "release/revision $revision/Morpheus (mzML Mono)"
+rsync -a --exclude=*.mdb "Morpheus/mzML/Mono/command line/bin/Release/." "release/revision $revision/Morpheus (mzML Mono)"
 
 cd "release/revision $revision"
-tar -czf "Morpheus (mzML Linux).tar.gz" "Morpheus (mzML Linux)"
+tar -czf "Morpheus_mzML_Mono.tar.gz" "Morpheus (mzML Mono)"
 
-rm -r "Morpheus (mzML Linux)"
+rm -rf "Morpheus (mzML Mono)"
+
