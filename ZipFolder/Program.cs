cb0b79b5d52efd353752193b0ef84e296a1c9535
Craig Wenger <cwenger@quantum-si.com>
2016-02-25 13:05:13 -0500

    Remove spaces from names of Windows .zip release files (modify ZipFolder to allow this). Add empty file with revision number to each release.

--- ZipFolder/Program.cs
+++ ZipFolder/Program.cs
@@ -7,8 +7,13 @@ namespace ZipFolder
     {
         static void Main(string[] args)
         {
-            string path = args[0];
-            ZipFile.CreateFromDirectory(path, Path.ChangeExtension(path, ".zip"), CompressionLevel.Optimal, true);
+            string input_path = args[0];
+            string output_path;
+            if (args.Length > 1)
+                output_path = args[1];
+            else
+                output_path = Path.ChangeExtension(input_path, ".zip");
+            ZipFile.CreateFromDirectory(input_path, output_path, CompressionLevel.Optimal, true);
         }
     }
 }
--- release.bat
+++ release.bat
@@ -10,19 +10,22 @@ mkdir "release\revision %revision%\Morpheus (mzML)"
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Morpheus\Morpheus (mzML).sln" /rebuild Release
 xcopy /y "Morpheus\mzML\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (mzML)"
 xcopy /y "Morpheus\mzML\command line\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (mzML)"
+copy /y NUL "release\revision %revision%\Morpheus (mzML)\revision %revision%" > NUL
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Morpheus\Morpheus (Agilent).sln" /rebuild "Release|x64"
 mkdir "release\revision %revision%\Morpheus (Agilent)"
 xcopy /y "Morpheus\Agilent\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (Agilent)"
 xcopy /y "Morpheus\Agilent\command line\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (Agilent)"
+copy /y NUL "release\revision %revision%\Morpheus (Agilent)\revision %revision%" > NUL
 mkdir "release\revision %revision%\Morpheus (Thermo)"
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Morpheus\Morpheus (Thermo).sln" /rebuild Release
 xcopy /y "Morpheus\Thermo\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (Thermo)"
 xcopy /y "Morpheus\Thermo\command line\bin\Release\*" /EXCLUDE:exclude.txt "release\revision %revision%\Morpheus (Thermo)"
+copy /y NUL "release\revision %revision%\Morpheus (Thermo)\revision %revision%" > NUL
 
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZipFolder\ZipFolder.sln /rebuild Release
-ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (mzML)"
-ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (Agilent)"
-ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (Thermo)"
+ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (mzML)" "release\revision %revision%\Morpheus_mzML.zip"
+ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (Agilent)" "release\revision %revision%\Morpheus_Agilent.zip"
+ZipFolder\bin\Release\ZipFolder.exe "release\revision %revision%\Morpheus (Thermo)" "release\revision %revision%\Morpheus_Thermo.zip"
 
 rmdir /s /q "release\revision %revision%\Morpheus (mzML)"
 rmdir /s /q "release\revision %revision%\Morpheus (Agilent)"
